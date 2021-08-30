37bd22c028a19fddef7b219e8fca9609cea8395c
Kevin Gagnon <kevin.gagnon@broadsign.com>
2021-08-30 14:36:26 -0400

    Finish Icons

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,17 +1,5 @@
 <template>
 	<div class="row full-width">
-		<!-- <div class="gallery col-6" v-touch-swipe.mouse.down="closeDrawer">
-			<q-img v-for="(hero, k) in content.gallery" :key="`hero_${k}`" :src="hero" :class="['hero', { active: galleryPos === k }]" />
-			<div class="thumbnails row justify-end">
-				<q-img
-					v-for="(thumb, j) in content.gallery"
-					:key="`tb_${j}`"
-					:src="thumb"
-					:class="['thumbnail', `tb_${j}`, { active: galleryPos === j }]"
-					@click="galleryPos = j"
-				/>
-			</div>
-		</div> -->
 		<q-carousel animated v-model="galleryPos" thumbnails infinite class="col-6" v-touch-swipe.mouse.down="closeDrawer">
 			<q-carousel-slide v-for="(hero, k) in content.gallery" :key="`hero_${k}`" :name="k" :img-src="hero" />
 		</q-carousel>
@@ -63,58 +51,6 @@ export default {
 	overflow: hidden;
 	transition: opacity 1s ease-out;
 	z-index: 1;
-
-	&.active {
-		opacity: 1;
-	}
-}
-
-.thumbnails {
-	position: absolute;
-	height: 72px;
-	bottom: 10px;
-	right: 10px;
-	width: 100%;
-	z-index: 10;
-}
-
-.thumbnail {
-	background-color: #000;
-	background-size: cover;
-	border: 3px solid #000;
-	border-radius: calc(min(2vw, 20px));
-	display: inline-block;
-	flex: 1 1 100%;
-	height: 72px;
-	margin-left: 10px;
-	cursor: pointer;
-	max-height: 7vmax;
-	max-width: 8vmax;
-	opacity: 1;
-	overflow: hidden;
-	padding: 0;
-	pointer-events: all;
-	width: 90px;
-
-	&.active {
-		border-color: #7bc;
-	}
-}
-
-h1 {
-	color: #83dbe7;
-	display: inline-block;
-	font-family: "Roboto Black", Arial Black, Helvetica, sans-serif;
-	font-size: 0.6em;
-	font-variant: small-caps;
-	left: -2px;
-	line-height: 80%;
-	margin: 0 0 0.5em;
-	position: relative;
-	text-transform: lowercase;
-	top: 0;
-	transform: none;
-	width: fit-content;
 }
 
 .content {
@@ -140,6 +76,35 @@ h1 {
 .client_slide {
 	user-select: none;
 
+	.q-carousel__navigation-inner {
+		justify-content: flex-end;
+		height: 72px;
+		max-height: 7vmax;
+	}
+
+	.q-carousel__thumbnail {
+		background-color: #000;
+		background-size: cover;
+		border: 3px solid #000;
+		border-radius: calc(min(2vw, 20px));
+		display: inline-block;
+		flex: 1 1 100%;
+		height: 72px;
+		margin-left: 10px;
+		cursor: pointer;
+		max-height: 7vmax;
+		max-width: 8vmax;
+		opacity: 1;
+		overflow: hidden;
+		padding: 0;
+		pointer-events: all;
+		width: 90px;
+
+		&--active {
+			border-color: #7bc;
+		}
+	}
+
 	.q-scrollarea {
 		height: 88vh;
 		max-width: 100%;
@@ -151,6 +116,7 @@ h1 {
 			justify-content: flex-start;
 		}
 	}
+
 	blockquote {
 		color: #83dbe7;
 		display: inline-block;
@@ -173,11 +139,27 @@ h1 {
 		}
 	}
 
+	h1 {
+		color: #83dbe7;
+		display: inline-block;
+		font-family: "Roboto Black", Arial Black, Helvetica, sans-serif;
+		font-size: 0.6em;
+		font-variant: small-caps;
+		left: -2px;
+		line-height: 80%;
+		margin: 0 0 0.5em;
+		position: relative;
+		text-transform: lowercase;
+		top: 0;
+		transform: none;
+		width: fit-content;
+	}
+
 	h2 {
 		color: #fff;
 		display: inline-block;
 		font-family: "Roboto Black", Arial Black, Helvetica, sans-serif;
-		font-size: 0.16em;
+		font-size: 0.22em;
 		margin: 0 0 0.5em;
 		line-height: 140%;
 		text-transform: uppercase;
@@ -187,9 +169,9 @@ h1 {
 		color: #fff;
 		display: inline-block;
 		font-family: "Roboto", Arial, Helvetica, sans-serif;
-		font-size: 0.13em;
+		font-size: 0.2em;
 		line-height: 150%;
-		margin: 0 0 2.4em;
+		margin: 0 0 1.6em;
 
 		&.slides_count {
 			bottom: 15px;
